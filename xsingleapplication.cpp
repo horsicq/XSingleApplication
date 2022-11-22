109d29be7f6124eabc561e22b46451977ce7bf57
horsicq <horsicq@gmail.com>
2022-11-22 05:23:40 +0100

    Fix: 2022-11-22

--- xsingleapplication.cpp
+++ xsingleapplication.cpp
@@ -20,8 +20,7 @@
  */
 #include "xsingleapplication.h"
 
-XSingleApplication::XSingleApplication(int &argc, char *argv[])
-    : QApplication(argc, argv)
+XSingleApplication::XSingleApplication(int &argc, char *argv[]) : QApplication(argc, argv)
 {
     g_pSharedMemory = nullptr;
     g_pLocalServer = nullptr;
